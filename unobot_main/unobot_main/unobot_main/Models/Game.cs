b3cc65773e73f895120db59295e7b064474f12b3
Conner Smith <connerpsmith@gmail.com>
2017-02-13 11:05:54 -0600

    addplayer method

--- unobot_main/unobot_main/unobot_main/Models/Game.cs
+++ unobot_main/unobot_main/unobot_main/Models/Game.cs
@@ -8,5 +8,16 @@ namespace unobot_main.Models
         public Deck Deck { get; set; }
         public IList<Hand> Hands { get; set; }
         public int Turn { get; set; }
+
+        public bool AddPlayer(Player player)
+        {
+            if (Players.Count >= 4)
+            {
+                return false;
+            }
+
+            Players.Add(player);
+            return true;
+        }
     }
 }
