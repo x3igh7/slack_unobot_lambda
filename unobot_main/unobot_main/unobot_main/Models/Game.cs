c37e0da336b30b6c864103774476933a3de6af9b
Conner Smith <connerpsmith@gmail.com>
2017-02-13 11:33:32 -0600

    create game method

--- unobot_main/unobot_main/unobot_main/Models/Game.cs
+++ unobot_main/unobot_main/unobot_main/Models/Game.cs
@@ -9,6 +9,16 @@ namespace unobot_main.Models
         public IList<Hand> Hands { get; set; }
         public int Turn { get; set; }
 
+        public void Create()
+        {
+            var deck = this.NewDeck();
+
+            this.Players = new List<Player>();
+            this.Deck = deck;
+            this.Hands = new List<Hand>();
+            this.Turn = 0;
+        }
+
         public bool AddPlayer(Player player)
         {
             if (this.Players.Count >= 4)
@@ -17,12 +27,20 @@ namespace unobot_main.Models
             }
 
             this.Players.Add(player);
-            this.Hands.Add(new Hand()
-            {
-                Player = player,
-                Cards = this.Deck.DealHand()
-            });
+            this.Hands.Add(
+                new Hand
+                {
+                    Player = player,
+                    Cards = this.Deck.DealHand()
+                });
             return true;
+        }
+
+        private Deck NewDeck()
+        {
+            var deck = new Deck();
+            deck.New();
+            return deck;
         }
     }
 }
