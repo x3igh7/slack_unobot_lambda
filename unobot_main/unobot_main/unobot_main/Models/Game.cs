96745b10d819bd2bcae230f9d83c8189c65beeb2
Conner Smith <connerpsmith@gmail.com>
2017-02-13 11:00:38 -0600

    adding models

---
+++ unobot_main/unobot_main/unobot_main/Models/Game.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace unobot_main.Models
+{
+    public class Game
+    {
+        public IList<Player> Players { get; set; }
+        public Deck Deck { get; set; }
+        public IList<Hand> Hands { get; set; }
+        public int Turn { get; set; }
+    }
+}
